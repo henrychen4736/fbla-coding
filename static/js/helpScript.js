21ccfde70f49f930eb1e3695c997d4a900aed2bd
Henry Chen <henrychen4736@gmail.com>
2024-02-21 16:27:47 -0800

    wrote some Q&A's for the help page, still needs more styling

--- static/js/helpScript.js
+++ static/js/helpScript.js
@@ -23,3 +23,47 @@ document.querySelectorAll('.accordion').forEach(button => {
         } 
     });
 });
+
+const toggleButton = document.querySelector('.fa-bars');
+const sidebar = document.querySelector('.sidebar');
+const sidebarOverlay = document.querySelector('.sidebar-overlay');
+const closeButton = document.querySelector('.fa-xmark');
+
+let sidebarOpen = false;
+
+function openSidebar() {
+    sidebarOpen = true;
+    sidebarOverlay.style.display = "block";
+    sidebarOverlay.style.width = "100vw";
+    sidebar.style.transform = "scale(1)";
+}
+
+function closeSidebar() {
+    sidebarOpen = false;
+    sidebarOverlay.style.display = "none";
+    sidebar.style.transform = "scale(0)";
+}
+
+toggleButton.addEventListener('click', function () {
+    if (!sidebarOpen) {
+        openSidebar();
+    } else {
+        closeSidebar();
+    }
+})
+
+sidebarOverlay.addEventListener('click', function () {
+    if (sidebarOpen) {
+        closeSidebar();
+    }
+})
+
+closeButton.addEventListener('click', function () {
+    closeSidebar();
+})
+
+document.querySelectorAll('.sidebar-item').forEach(item => {
+    item.addEventListener('click', function () {
+        closeSidebar();
+    });
+});
